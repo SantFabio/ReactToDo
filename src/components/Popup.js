215450debb28ac4a5871131b9e1a446418406cb7
Luiz Fábio Pereira Santana <118360185+SantFabio@users.noreply.github.com>
2023-12-15 13:59:42 +0000

    dando continuidade a logica do componente popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import Card from './Card'; // Ajuste o caminho conforme necessário
+import Card from './Card';
 
 export default function Popup(props) {
     const [show, setShow] = useState(false);
-
     useEffect(() => {
-        // Atualiza o estado com base na condição props.items.length > 1
-        setShow(props.items.length > 1);
+        if (props.items.length > 1 ) {
+            setShow(true)
+            localStorage.setItem("marker", true)
+            let interval = setInterval(() => {
+                setShow(false)
+                clearInterval(interval);
+            }, 5000);
+        }
+        console.log(true);
     }, [props.items]);
 
-    // Renderiza o componente com base no estado show
     return (
         <Card className={show ? "popup show" : "popup"}>
             <span>
                 Drag and drop to reorder list
             </span>
         </Card>
-    ) 
+    )
 }
